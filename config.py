def2539148e89416e842affd22e3b488690dcc49
asime204 <azielsime1@gmail.com>
2023-01-17 21:44:52 -0500

    basic flask app

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__name__))
 
-
 class Config():
     FLASK_APP = os.environ.get('FLASK_APP')
-    FLASK_ENV = os.environ.get('FLASK_ENV')
+    FLASK_ENV = os.environ.get("FLASK_ENV")
+    
