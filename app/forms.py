31122f1344c7d1e60c695c9e48aa30b8579f5368
asime204 <azielsime1@gmail.com>
2023-01-19 22:06:10 -0500

    login/logout, creating, retrieving updating deleting

--- app/forms.py
+++ app/forms.py
@@ -8,3 +8,16 @@ class UserCreationForm(FlaskForm):
     password = PasswordField("Password", validators = [DataRequired()])
     confirm_password = PasswordField("Confirm Password", validators = [DataRequired(), EqualTo('password')])
     submit = SubmitField()
+
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators = [DataRequired()])
+    password = PasswordField("Password", validators = [DataRequired()])
+    submit = SubmitField()
+
+class PostForm(FlaskForm):
+    title = StringField("Title", validators = [DataRequired()])
+    img_url = StringField("Image URL", validators = [DataRequired()])
+    caption = StringField("Caption", validators = [])
+    
+    submit = SubmitField()
